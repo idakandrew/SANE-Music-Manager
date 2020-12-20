ec91ff7d2f21205d24b5156882ef07f2dde29afd
Andrew Idak <75957379+idakandrew@users.noreply.github.com>
2020-12-20 14:53:02 -0500

    Update Analytic Queries.sql

--- Analytic Queries.sql
+++ Analytic Queries.sql
@@ -1,4 +1,4 @@
-// Mean album length in user library.
+-- Mean album length in user library.
 
 SELECT avg(sum_len)
 FROM
@@ -9,14 +9,14 @@ AND owns_album.album_id = album.album_id
 AND album.album_id = has.album_id AND has.song_id = song.song_id
 GROUP BY album.album_name) AS abc
 
-// Mean length of individual songs in user library.
+-- Mean length of individual songs in user library.
 
 SELECT ceil(avg(song.length)) AS avg_length
 FROM users, owns_song, song
 WHERE users.user_id = owns_song.user_id AND owns_song.song_id = song.song_id
 AND users.user_id = {current user}
 
-// Current user's 5 most owned genres.
+-- Current user's 5 most owned genres.
 
 SELECT has_genre_song.genre_type, count(has_genre_song.genre_type)
 FROM users, owns_song, song, has_genre_song
@@ -26,7 +26,7 @@ GROUP BY has_genre_song.genre_type
 ORDER BY count(has_genre_song.genre_type) DESC
 LIMIT 5
 
-// Current user's 5 most played artists.
+-- Current user's 5 most played artists.
 
 SELECT artist.artist_name, count(artist.artist_id)
 FROM users, owns_song, song, created_by, artist
@@ -37,7 +37,7 @@ GROUP BY artist.artist_name
 ORDER BY count(artist.artist_id) DESC
 LIMIT 5
 
-// Current user's 5 most played songs.
+-- Current user's 5 most played songs.
 
 SELECT song.title, owns_song.play_count
 FROM users, owns_song, song
@@ -46,7 +46,7 @@ AND users.user_id = {current user}
 ORDER BY owns_song.play_count DESC
 LIMIT 5
 
-// Current user's 5 most played genres.
+-- Current user's 5 most played genres.
 
 SELECT has_genre_song.genre_type, count(has_genre_song.genre_type)
 FROM users, owns_song, song, has_genre_song
@@ -56,7 +56,7 @@ GROUP BY has_genre_song.genre_type
 ORDER BY count(has_genre_song.genre_type) DESC
 LIMIT 5
 
-// Genre ownership totals amongst all users.
+-- Genre ownership totals amongst all users.
 
 SELECT has_genre_song.genre_type, count(has_genre_song.genre_type)
 FROM users, owns_song, song, has_genre_song
