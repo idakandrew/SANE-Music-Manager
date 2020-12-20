ab355abcb722f9c989c1936a6559631bc563212b
Andrew Idak <75957379+idakandrew@users.noreply.github.com>
2020-12-20 14:55:26 -0500

    Update and rename Retrieval Queries to retrieval_queries.sql

--- retrieval_queries.sql
+++ retrieval_queries.sql
@@ -1,11 +1,11 @@
-// Find song from GUI search function.
+-- Find song from GUI search function.
 
 select song.* from song where lower(song.title) like '%<search>%';
 
-// Find album from GUI search function.
+-- Find album from GUI search function.
 
 select album.* from album where lower(album_name) like '%<search>%';
 
-// Select user based on given ID.
+-- Select user based on given ID.
 
 select * from users where user_id=<id>;
